7cc6edb42c5329a53bf4098bfebff4fb51fcddc0
Mandera <krizoorn@hotmail.com>
2020-05-06 18:27:57 +0200

    Changed version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name = "generallibrary",
-    version = "1.6.0",
+    version = "1.7.0",
     description = (""
-                   "Added tests. "
+                   "Added None support and changed bool to not be int."
                    " Random functions to help with native classes."
                    ""),
     packages = find_packages(),
