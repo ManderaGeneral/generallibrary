a1d0d382e98d56df1074f2016c295ebee28f8708
Mandera <krizoorn@hotmail.com>
2020-07-06 12:36:42 +0200

    Timer test failed, decreased by one decimal point

--- test/test_time.py
+++ test/test_time.py
@@ -7,7 +7,7 @@ from generallibrary.time import Timer, sleep
 class TimeTest(unittest.TestCase):
     def test_timerAndSleep(self):
         timer = Timer()
-        sleep(0.05)
+        sleep(0.1)
         secs = timer.seconds()
-        self.assertEqual(round(secs, 2), 0.05)
+        self.assertEqual(round(secs, 1), 0.1)
         self.assertGreater(Timer(0).seconds(), 1588527842)
