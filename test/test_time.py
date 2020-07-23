92a3f5c879191a62c7d9f3db9a99d24daf32f3ab
Mandera <krizoorn@hotmail.com>
2020-07-23 14:27:43 +0200

    Changed timer test to allow range instead.
    Removed walrus operators to support 3.7.
    Added travis.

--- test/test_time.py
+++ test/test_time.py
@@ -9,5 +9,5 @@ class TimeTest(unittest.TestCase):
         timer = Timer()
         sleep(0.1)
         secs = timer.seconds()
-        self.assertEqual(round(secs, 1), 0.1)
+        self.assertTrue(0.09 < secs < 0.2)
         self.assertGreater(Timer(0).seconds(), 1588527842)
