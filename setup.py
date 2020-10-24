bab3678dda3408d8445b6fa979286a6d3be799b1
Mandera <krizoorn@hotmail.com>
2020-10-24 13:43:07 +0200

    Real test

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type='text/markdown',
     author='Rickard "Mandera" Abraham',
     url="https://github.com/ManderaGeneral/generallibrary",
-    version="2.1.11",
+    version="2.1.12",
     description=(
         "Random useful code made by me, categorized into modules to be imported seperately."
     ),
