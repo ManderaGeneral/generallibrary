84ddfea3c9fbc72706337c928c370eae3f36e2e1
Mandera <krizoorn@hotmail.com>
2020-10-30 22:30:41 +0100

    [CI SKIP] Testing cfg

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+
 setup(
     name="generallibrary",
     long_description=long_description,
