a1b38d23c0b094934fc26c319b62cd0508bba33c
Mandera <krizoorn@hotmail.com>
2020-11-16 12:41:27 +0100

    New hook technique

--- generallibrary/diagram.py
+++ generallibrary/diagram.py
@@ -1,19 +1,16 @@
 
 from generallibrary import SigInfo
 
+
 class TreeDiagram:
     """ Saveable tree diagram with optional storage.
         Inherit TreeDiagram and define what keys to store in `data_keys`.
 
         Saves class name and has to access it as an attribute when using `load()`.
         Use metaclass generallibrary.HierarchyStorer to easily store inheriters base class.
-        Use initBases decorator to automatically call _post_init. """
+        Use initBases decorator to automatically call _post_init.
+        Todo: TnD """
     data_keys = []
-
-    hook_create, hook_remove = None, None
-    hook_new_parent, hook_lose_parent = None, None
-    hook_add_child, hook_lose_child = None, None
-    hook_set_attribute = None
 
     def __init__(self, parent=None, children_dicts=None):
         self.children = []
@@ -27,28 +24,32 @@ class TreeDiagram:
             for child_dict in children_dicts:
                 self.load(child_dict, parent=self)
 
-    def _post_init(self):
-        self.call_hook(self.hook_create)
+    def hook_create(self): pass
+    def hook_remove(self): pass
+    def hook_new_parent(self, parent, old_parent): pass
+    def hook_lose_parent(self, old_parent, parent): pass
+    def hook_add_child(self, child): pass
+    def hook_lose_child(self, child): pass
+    def hook_set_attribute(self, key, value, old_value): pass
 
-    def call_hook(self, hook, *args, **kwargs):
-        if hook:
-            SigInfo(hook, *args, **kwargs)()
+    def _post_init(self):  # @initBases calls this automatically
+        self.hook_create()
 
     def set_parent(self, parent):
         old_parent = self.get_parent()
         if old_parent:
             old_parent.children.remove(self)
 
-            old_parent.call_hook(old_parent.hook_lose_child, self)
-            self.call_hook(self.hook_lose_parent, old_parent, parent)
+            old_parent.hook_lose_child(child=self)
+            self.hook_lose_parent(old_parent=old_parent, parent=parent)
 
         if parent:
             if self in parent.all_parents():
                 raise AttributeError(f"Cannot set {parent} as parent for {self} as it becomes circular. ")
             parent.children.append(self)
 
-            parent.call_hook(parent.hook_add_child, self)
-            self.call_hook(self.hook_new_parent, parent, old_parent)
+            parent.hook_add_child(self)
+            self.hook_new_parent(parent=parent, old_parent=old_parent)
 
         self._parent = parent
         return self
@@ -86,7 +87,7 @@ class TreeDiagram:
 
     def remove(self):
         self.set_parent(None)
-        self.call_hook(self.hook_remove)
+        self.hook_remove()
 
     def __repr__(self):
         return f"<{self.__class__.__name__} {getattr(self, 'children', '')}>"
@@ -95,7 +96,7 @@ class TreeDiagram:
         if key in self.data_keys:
             old_value = self.data.get(key)
             self.data[key] = value
-            self.call_hook(self.hook_set_attribute, key, value, old_value)
+            self.hook_set_attribute(key=key, value=value, old_value=old_value)
         object.__setattr__(self, key, value)
 
 
