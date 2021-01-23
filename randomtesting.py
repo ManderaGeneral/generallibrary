d5c9e133e3ff0df7c8bc02acce46a7dfb8c8770d
Mandera <rickard.abraham@gmail.com>
2021-01-23 17:51:02 +0100

    [CI SKIP] Lovely get_routes for Node

--- randomtesting.py
+++ randomtesting.py
@@ -1,10 +1,24 @@
 
 from generallibrary import *
 
-from itertools import chain
 
+# HERE ** Add methods to get nodes and links easily from Route
+# Combine with TreeDiagram to share inheritence
 
-# HERE ** Rough sketch for NetworkDiagram, eventually combine with TreeDiagram to share inheritence
+class Route(list):
+    """ List of nodes. """
+    def copy(self):
+        return Route(self)
+
+    def is_circular(self):
+        return self[-1] in self
+
+    def is_uturn(self):
+        return len(self) > 2 and self[-1] is self[-3]
+
+    def __repr__(self):
+        return f"{type(self).__name__}: {super().__repr__()}{' ⟲' * self.is_circular()}{' ⮌' * self.is_uturn()}"
+
 
 class Link:
     def __init__(self, base, target):
@@ -34,8 +48,6 @@ class Node:
         target.links.append(link)
 
 
-
-
     def _get_active_links(self, incoming, outgoing):
         return [link for link in self.links if (incoming and link.target is self) or (outgoing and link.base is self)]
 
@@ -45,22 +57,30 @@ class Node:
         connected_nodes = []
         for link in active_links:
             other_node = link.other_node(self)
-            if other_node not in route:
-                connected_nodes.append(other_node)
+            # if other_node not in route:
+            connected_nodes.append(other_node)
         return connected_nodes
 
-    def get_nodes(self, depth=1, incoming=True, outgoing=True, combined=True, _route=None):
+    def get_routes(self, depth=-1, incoming=False, outgoing=True, _route=None, _all_routes=None):
+        """ Get a list of routes. """
         if _route is None:
-            _route = []
-        _route.append(self)
+            _route = Route()
+            _all_routes = [_route]
 
-        if depth == 0:
-            return _route
+        _route.append(self)
+        if depth == 0 or _route.count(self) > 1:
+            return _all_routes
 
         connected_nodes = self._get_connected_nodes(route=_route, incoming=incoming, outgoing=outgoing)
-        if connected_nodes:
-            connected_nodes[0].get_nodes(depth=depth - 1, incoming=incoming, outgoing=outgoing, combined=combined, _route=_route)
-        return _route
+        _original_route = _route.copy()
+        for i, node in enumerate(connected_nodes):
+            if i == 0:
+                new_route = _route
+            else:
+                new_route = _original_route.copy()
+                _all_routes.append(new_route)
+            node.get_routes(depth=depth - 1, incoming=incoming, outgoing=outgoing, _route=new_route, _all_routes=_all_routes)
+        return _all_routes
 
 
 a = Node(1)
@@ -73,7 +93,10 @@ b.link(c)
 c.link(d)
 d.link(b)
 
-print(b.get_nodes(depth=-1))
+print(a.get_routes())
+
+
+
 
 
 
