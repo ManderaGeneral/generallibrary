747fdfa9254716dc8b6323898d5b811476c0addf
runner <runner@fv-az284-228>
2022-10-07 14:07:10 +0000

    [CI AUTO] Sync triggered by generalpackager

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name="generallibrary",
     author='Rickard "Mandera" Abraham',
     author_email="rickard.abraham@gmail.com",
-    version="2.9.9",
+    version="2.9.10",
     description="Random useful code categorized into modules.",
     long_description=long_description,
     long_description_content_type="text/markdown",
