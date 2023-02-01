dc1a1cfff0a7187332e52f282099ec4137dedca9
Mandera <rickard.abraham@gmail.com>
2023-02-01 21:50:55 +0700

    First push to two repos at once! #17
    Seems that they both share the same commit message? Makes sense.

--- randomtesting.py
+++ randomtesting.py
@@ -8,7 +8,6 @@ from generallibrary import *
 # print(depth({'test': {'foo': 'bar', 'number': 2, 'hi': ['a', 'b', 3], "uh": None}}))
 
 
-
 # print(Terminal("-c", "print(5)", python=True, error=False, capture_output=False))
 
 print(Terminal("-c", "assert 4 == 5", python=True, capture_output=True, error=False).string_result)
