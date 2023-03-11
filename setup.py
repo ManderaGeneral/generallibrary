f7088172dc715500341689eacbf3075dfb7675cd
Mandera <rickard.abraham@gmail.com>
2023-03-11 18:45:49 +0800

    [CI SKIP] Changed version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name="generallibrary",
     author='Rickard "Mandera" Abraham',
     author_email="rickard.abraham@gmail.com",
-    version="2.10.0",
+    version="2.9.12",
     description="Random useful code categorized into modules.",
     long_description=long_description,
     long_description_content_type="text/markdown",
