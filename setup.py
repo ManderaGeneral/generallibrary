8548b0163c34e00734e35be3ba06505b93683e99
Mandera <rickard.abraham@gmail.com>
2024-01-24 08:13:03 +0200

    Publish new optional dep

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name="generallibrary",
     author='Rickard "Mandera" Abraham',
     author_email="rickard.abraham@gmail.com",
-    version="2.9.14",
+    version="2.10.0",
     description="Random useful code categorized into modules.",
     long_description=long_description,
     long_description_content_type="text/markdown",
